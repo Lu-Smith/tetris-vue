8dbd3dfdaffd32a24abbe92749231839361a7abc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-10 13:52:37 +0100

    Created AssetsComponent.vue

---
+++ src/components/AssetsComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="assets">
+        <audio :src=Lost id="lose"></audio>
+        <audio :src=Crack id="crack"></audio>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import Crack from '../assets/media/crack.wav';
+    import Lost from '../assets/media/lose.wav';
+</script>
