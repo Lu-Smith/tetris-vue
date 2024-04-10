76ab938307dd919491729be09ce378bfd020bd40
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-10 13:52:58 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-    Tetris
+  <div class="App">
+    <h1 v-if="!gameRunning">Tetris</h1>
+    <DescriptionComponent v-if="!gameRunning" />
+    <button @click="startNewGame" v-if="!gameRunning">Start</button>
+    <MainGame v-if="gameRunning" :gameRunning="gameRunning" @toggleHome="toggleHome"/>
+    <InstructionComponent v-if="!gameRunning" />
+    <FooterComponent v-if="!gameRunning" />
   </div>
 </template>
+
+<script setup lang="ts">
+  import FooterComponent from './components/FooterComponent.vue';
+  import DescriptionComponent from './components/DescriptionComponent.vue';
+  import InstructionComponent from './components/InstructionComponent.vue';
+  import MainGame from './components/MainGame.vue';
+
+  import { ref } from 'vue';
+
+  const gameRunning = ref(false);
+
+  const startNewGame = () => {
+    gameRunning.value = !gameRunning.value;
+  }
+
+  const toggleHome = () => {
+    gameRunning.value = false;
+  }
+</script>
