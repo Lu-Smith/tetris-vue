a01b7fb787156923e9c40a87faa04e60079f97ef
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-10 18:07:14 +0100

    Updated block.ts

--- src/assets/classes/block.ts
+++ src/assets/classes/block.ts
@@ -2,24 +2,36 @@ import Game from './game';
 
 export default class Block {
     game: Game;
+    side: number;
     x: number;
     y: number;
-    side: number;
+    height: number;
+    weight: number;
 
     constructor (game: Game) {
         this.game = game;
-        this.side = 10;
+        this.side = 20;
         this.x = (this.game.canvas.width * 0.5 - this.side * 0.5);
-        this.y = 35;
+        this.y = 25;
+        this.height = this.game.background.scaledHeight + 62 - this.side;
+        this.weight = this.game.background.scaledWidth * 0.5 - this.side;
         
+    }
+    update() {
+        if (this.y < this.height) this.y += this.game.speed;
     }
     draw(context: CanvasRenderingContext2D){
         context.save();
-        context.fillStyle = 'white';
+        context.fillStyle = 'blue';
+        context.lineWidth = 2;
+        context.strokeStyle = 'black';
         context.fillRect(this.x, this.y, this.side, this.side);
+        context.strokeRect(this.x, this.y, this.side, this.side);
         context.restore();
     }
     resize() {
         this.x = (this.game.canvas.width * 0.5 - this.side * 0.5);
+        this.height = this.game.background.scaledHeight + 62 - this.side;
+        this.weight = this.game.background.scaledWidth * 0.5 - this.side;
     }
 }
