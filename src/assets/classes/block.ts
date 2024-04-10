c4294bce1c4ca6dbf677f592a6e4f4936ade9e2f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-10 17:16:50 +0100

    Updated block.ts

--- src/assets/classes/block.ts
+++ src/assets/classes/block.ts
@@ -8,16 +8,18 @@ export default class Block {
 
     constructor (game: Game) {
         this.game = game;
-        this.x = 0;
-        this.y = 0;
         this.side = 10;
+        this.x = (this.game.canvas.width * 0.5 - this.side * 0.5);
+        this.y = 35;
+        
     }
-
     draw(context: CanvasRenderingContext2D){
         context.save();
-
         context.fillStyle = 'white';
         context.fillRect(this.x, this.y, this.side, this.side);
         context.restore();
+    }
+    resize() {
+        this.x = (this.game.canvas.width * 0.5 - this.side * 0.5);
     }
 }
