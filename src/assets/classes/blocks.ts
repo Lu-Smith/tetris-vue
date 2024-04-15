492e7d1765ca3f90e98e9ebce67e8f8687a5c4fc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-04-15 12:21:41 +0100

    Updated block.ts

--- src/assets/classes/blocks.ts
+++ src/assets/classes/blocks.ts
@@ -78,10 +78,14 @@ export default class Blocks {
     }
     calculateCoveredCells() {
         const coveredCells: number[][] = [];
-        const startX = Math.floor((this.x - this.game.blockSize * 0.5) / this.game.blockSize);
-        const endX = Math.floor((this.x + this.width - this.game.blockSize * 0.5) / this.game.blockSize);
-        const startY = Math.floor(this.y / this.game.blockSize);
-        const endY = Math.floor((this.y + this.height) / this.game.blockSize);
+        const startX = Math.floor((this.x - this.game.blockSize * 0.5) / this.game.blockSize
+    - (this.game.canvas.width * 0.5 - this.game.background.scaledWidth * 0.5) / this.game.blockSize);
+        const endX = Math.floor((this.x + this.width - this.game.blockSize * 0.5) / this.game.blockSize
+    - (this.game.canvas.width * 0.5 - this.game.background.scaledWidth * 0.5) / this.game.blockSize);
+        const startY = Math.floor(this.y / this.game.blockSize - 65 / this.game.blockSize);
+        const endY = Math.floor((this.y + this.height - 65) / this.game.blockSize);
+
+        console.log(startX, endX);
 
         for (let y = startY; y < endY; y++) {
             for (let x = startX; x < endX; x++) {
@@ -90,12 +94,13 @@ export default class Blocks {
         }
 
         for (let [y, x] of coveredCells) {
-            this.rowIndex = y - 3;
-            this.columnIndex = x - 2;
+            this.rowIndex = y + 1;
+            this.columnIndex = x;
             this.game.grid[this.rowIndex][this.columnIndex] = 0;
             this.game.minBottoms[this.columnIndex] -= this.game.blockSize;
             this.bottom = this.game.minBottoms[this.columnIndex];
         } 
+        console.log(coveredCells);
     }
     render(context: CanvasRenderingContext2D) {
             if ((this.game.keys.indexOf('ArrowDown') > -1)) {
@@ -103,6 +108,7 @@ export default class Blocks {
             } else {
                 this.speedY = this.game.speed;
             }
+
     
         this.blocks.forEach(block => {
             if (this.y < this.bottom - this.height) { 
@@ -112,20 +118,20 @@ export default class Blocks {
                 //vertical boundries
                 this.speedY = 0;
                 this.y = this.bottom;
+              
             }
             block.update(this.x, this.y);
             block.draw(context);
+    
         })
 
-        console.log(this.game.minBottoms);
+        // console.log(this.game.minBottoms);
 
         if (!this.nextBlockTrigger && this.speedY === 0) {
-            setTimeout(() => {
-                if (!this.game.eventUpdate) {
-                    this.calculateCoveredCells();
-                    this.game.newBlock(); 
-                }
-            }, 1000);
+            this.calculateCoveredCells();
+            if (!this.game.eventUpdate) {
+                this.game.newBlock(); 
+            }
             this.nextBlockTrigger = true;
         }
     }
